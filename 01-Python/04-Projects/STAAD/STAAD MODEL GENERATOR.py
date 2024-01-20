fde7a56ac6dc36f110bea1774cd90248849d6b18
Balaji-Kannigesvaran <krishbala49@gmail.com>
2024-01-20 13:22:29 +0530

    Learning before removing

--- 01-Python/04-Projects/STAAD/STAAD MODEL GENERATOR.py
+++ 01-Python/04-Projects/STAAD/STAAD MODEL GENERATOR.py
@@ -8,27 +8,27 @@ B = 100
 EC_Ht = 12
 Slope = 20
 RC_Ht = (B/Slope)+EC_Ht
-CS_L=10                                                                         #Column Spacing along Length
-CS_B=10                                                                         #Column Spacing along Width
-NC_L=(L/CS_L)+1                                                                 #No of columns along Length
-NC_B=(B/CS_B)+1                                                                 #No of columns along Width
-print ('Joint Coordinates')
-JC=np.empty()
-ORIGIN=1
-for i in range (0,L,CS_L):
-    for j in range (0,B,CS_B):
-        NN=str(ORIGIN)
-        JC_X=str(i)
-        JC_Y=str((j/Slope)+EC_Ht)
-        JC_Z=str(j)
-        ORIGIN=ORIGIN+1
+CS_L = 10                                                                         #Column Spacing along Length
+CS_B = 10                                                                         #Column Spacing along Width
+NC_L = int((L/CS_L)+1)                                                            #No of columns along Length
+NC_B = int((B/CS_B)+1)                                                            #No of columns along Width
+print('Joint Coordinates')
+JC = np.empty((NC_L, NC_B))
+ORIGIN = 1
+for i in range(0, L, CS_L):
+    for j in range (0, B, CS_B):
+        NN = str(ORIGIN)
+        JC_X = str(i)
+        JC_Y = str((j/Slope)+EC_Ht)
+        JC_Z = str(j)
+        ORIGIN = ORIGIN+1
         JC1 = (NN + " " + JC_X + " " + JC_Y + " " + JC_Z + ";")
-        JC=np.append(JC,JC1)
-        NN=str(ORIGIN)
-        JC_X=str(i)
-        JC_Y=str(0)
-        JC_Z=str(j)
-        ORIGIN=ORIGIN+1
+        JC = np.append(JC, JC1)
+        NN = str(ORIGIN)
+        JC_X = str(i)
+        JC_Y = str(0)
+        JC_Z = str(j)
+        ORIGIN = ORIGIN+1
         JC2 = (NN + " " + JC_X + " " + JC_Y + " " + JC_Z + ";")
-        JC=np.append(JC,JC2)
+        JC = np.append(JC,JC2)
 print (JC)
